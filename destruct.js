d9d77fddc1951c52ec0600266a65c02c0a458023
Maria Lazaro <maria.lazaro.fox@gmail.com>
2022-05-04 16:16:37 -0700

    init repository

---
+++ destruct.js
@@ -0,0 +1,41 @@
+// Object Destructuring 1 returns:
+//  8
+// 1846
+
+// Object Destructuring 2 prints:
+// yearNeptuneDiscovered: 1846,
+// yearMarsDiscovered: 1659
+
+//Object Deconstructuring 3
+// "Your name is Alejandro and you like purple"
+// "Your name is Melissa and you like green"
+// Your name is undefined and you like green.
+
+//Array Destr. 1
+// "Maya"
+// "Marisa"
+// "Chi"
+
+//Array Destr. 2
+// "Raindrops on roses",
+
+// "whiskers on kittens",
+
+//["Bright copper kettles", "warm woolen mittens","Brown paper packages tied up with strings"]
+
+//Array Destr. 3
+// [10,30,20]
+// GOING BY INDEX NUMBER, 20, 30 = 30, 20, swapping.
+
+//ES2015 Refactoring
+
+// const {a,b} = obj.numbers
+
+//Array Swap
+// [arr[0], arr[1] = arr[1],arr[0]];
+
+//raceResults()
+
+// const raceResults = ([first, second, third, ...rest]) => {
+//   return { first, second, third, rest };
+// };
